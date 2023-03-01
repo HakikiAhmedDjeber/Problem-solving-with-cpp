e153022a9e1b66c17470802e10ea439dd1e70d78
Hakiki Ahmed Djeber <Hakikiahmeddjeber@gmail.com>
2023-03-01 17:16:23 +0100

    copy the hard compare problem and understand the solution

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 // 35 i can't do it so i see the correction from youtube playlist
 void lastTwoDigits(){
@@ -12,7 +13,18 @@ void lastTwoDigits(){
     if(res%100 <= 9) cout << "0" << res % 100;
     else cout << res % 100;
 }
+// 36
+// i had the idea of reducing and log but i can't do it myself
+void hardCompare(){
+    long double a,b,c,d;
+    cin >> a >> b >> c >> d;
+    a = b*log(a);
+    c = d*log(c);
+    if(a>c)cout << "YES";
+    else cout << "NO";
+}
 int main() {
-    lastTwoDigits();
+    //lastTwoDigits();
+    hardCompare();
     return 0;
 }
