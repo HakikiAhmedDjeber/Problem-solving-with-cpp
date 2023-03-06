0747084fe5651cddbf7b2f1e4b334b6f1c1cb3c2
Hakiki Ahmed Djeber <Hakikiahmeddjeber@gmail.com>
2023-03-06 21:14:04 +0100

    do memo and momo i had a runtime error cause of using small type variables

--- main.cpp
+++ main.cpp
@@ -32,9 +32,21 @@ void winterSales(){
     price = p / x;
     printf("%.2lf",price);
 }
+// 38
+void memoAndMomo(){
+    long long a,b,k;
+    cin >> a >> b >> k;
+    cout << a << endl << b << endl << k << endl;
+    if(a % k == 0 && b %k == 0) {
+        cout << "Both";
+    }else if(a%k==0)cout << "Memo";
+    else if (b%k==0)cout << "Momo";
+    else cout << "No One";
+}
 int main() {
     //lastTwoDigits();
     //hardCompare();
-    winterSales();
+    //winterSales();
+    memoAndMomo();
     return 0;
 }
